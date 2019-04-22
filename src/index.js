d48563ace547e75e9004d25d08ab42761893c1dc
kumavis <aaron@kumavis.me>
2019-04-22 13:55:30 +0800

    export util

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const { setupSimulation, setupSimulationForces } = require('./simulation')
 const renderGraph = require('./normal')
 const ForceGraph = require('./ForceGraph')
 const GraphContainer = require('./GraphContainer')
+const util = require('./util')
 
 module.exports = {
   ForceGraph, GraphContainer,
-  buildGraph, mergeGraph, setupSimulation, setupSimulationForces, renderGraph  
+  buildGraph, mergeGraph, setupSimulation, setupSimulationForces, renderGraph, util
 }
