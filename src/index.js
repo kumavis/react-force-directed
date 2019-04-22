502a594521be5338e06cebdb1582fef4a9fd93ef
kumavis <aaron@kumavis.me>
2019-04-22 13:44:13 +0800

    prepare module to export es5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const { buildGraph, mergeGraph } = require('./build')
 const { setupSimulation, setupSimulationForces } = require('./simulation')
 const renderGraph = require('./normal')
+const ForceGraph = require('./ForceGraph')
+const GraphContainer = require('./GraphContainer')
 
 module.exports = {
+  ForceGraph, GraphContainer,
   buildGraph, mergeGraph, setupSimulation, setupSimulationForces, renderGraph  
 }
